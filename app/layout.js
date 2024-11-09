821edec66504e71edb57ae77d97d08eebe6c6ad9
Md. Mamun <mkmamun031@gmail.com>
2024-11-09 11:35:52 +0600

    :bulb:Added React Toast

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import { ClerkProvider } from '@clerk/nextjs'
+import { ToastProvider } from "@/components/providers/toaster-provider";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({ children }) {
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <ToastProvider />
           {children}
         </body>
       </html>
